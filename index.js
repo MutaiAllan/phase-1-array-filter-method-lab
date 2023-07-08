21af7e53c50ff7913ec44d8fa13907afc2315123
MutaiAllan <MutaiAllan>
2023-07-08 12:24:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, string){
+    const driverName = string.toLowerCase();
+    const matching = drivers.filter(driver => driver.toLowerCase().includes(driverName));
+    return matching;
+}
+
+function fuzzyMatch(drivers, string){
+    const letters = string.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters));
+}
+
+function matchName(drivers, string){
+    return drivers.filter(driver => driver.name === string);
+}
